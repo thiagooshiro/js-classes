356f70daaf8511d2b9eaf49cca080fc528ff978c
Thiago Oshiro <85767209+thiagooshiro@users.noreply.github.com>
2024-04-29 12:22:48 -0300

    Add files via upload

---
+++ javascript-classes/aula5/aula5.js
@@ -0,0 +1,29 @@
+let meus_filmes = ['Homem Aranha', 'A Múmia', 'O Sexto sentido']
+
+
+// for(let i = 0; i < meus_filmes.length; i+=1) {
+//     console.log(meus_filmes[i])
+// }
+
+let names = ["Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona", "George", "Hannah", "Ian", "Julia"];
+
+// for (let i = 0; i < names.length; i+=1){
+//     // console.log(names[i]) // Um nome da lista
+//     let nome = names[i]
+//     // console.log(nome[0])
+//     if (nome[0] === 'C') {
+//         console.log('nomes[i]:', names[i])
+//         console.log('nome:', nome)
+//         }
+
+// }
+
+names.push('Douglas')
+names.unshift('Lucas')
+
+names.pop()
+
+console.log(names)
+
+names.splice()
+
