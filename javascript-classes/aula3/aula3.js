d6a3f70d4292dc91b49e92359f4605b98c7867ac
thiagooshiro <oshirothiago@gmail.com>
2024-05-31 18:41:48 -0300

    aula3 exercises corrected

---
+++ javascript-classes/aula3/aula3.js
@@ -0,0 +1,11 @@
+// Estrutura sintática do While
+condicao = 3
+
+while (condicao > 0) {
+    console.log('Enquanto a condição for verdadeira, o while executara os comandos dentro das chaves que o delimitam')
+    console.log(`Essa linha será executada`)
+    condicao -= 1
+} 
+
+console.log('Quando a condição não é mais verdadeira o script continua sua execução normal.')
+
