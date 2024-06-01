e0669236ebe84816ebcefd3bff0783ca54937e8a
thiagooshiro <oshirothiago@gmail.com>
2024-06-01 10:29:58 -0300

    aula3 exercises

---
+++ javascript-classes/aula3/Ex02.js
@@ -0,0 +1,10 @@
+// 2-  Crie um programa que dado um número (ex: 7), imprime todos os valores entre esse número e 0;
+
+
+let numero = 12
+let contador = 1
+
+while (contador < numero) {
+    console.log(contador)
+    contador += 1
+}
