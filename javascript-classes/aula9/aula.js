51bbd8421ebbceccb6e6ef726191a9f19780f478
thiagooshiro <oshirothiago@gmail.com>
2024-06-17 02:48:12 -0300

    aula9 implementada

---
+++ javascript-classes/aula9/aula.js
@@ -0,0 +1,33 @@
+//Desafio: Crie um arquivo script para a pagina "challenge.html" e implemente as funções necessarias para torná-la funcional
+
+
+//Requisito 1: Verificação de login e a senha.
+//Defina qual ou quais são as senhas e emails corretos 
+//Caso o usuario acerte a senha exiba a mensagem "Login bem sucedido"
+//Caso a senha esteja incorreta exiba a mensagem "Senha incorreta, tente novamente"
+
+
+// Requisito 2: Melhore experiência do usuário na página.
+// Quando o usuário mantiver seu mouse sobre um dos planos esse deve ficar em destaque.
+// Quando o usuário passar o mouse sobre o botão assinar ou contato, este deve ser destacado de alguma forma.
+// Substitua o elemento planos de assinatura por uma imagem com o logo do seu produto.
+
+
+//Requisito 3: Implemente as funções dos botões de assinatura;
+//Cada botão clicado deve enviar o usuario para uma página de formulário.
+//Antes desse redirecionamento ela deve imprimir no console qual plano foi escolhido
+
+
+//Requisito 4: Estilização e customização.
+// Customize a página, alterando as propriedadesde CSS da página para que estejam mais de acordo com o que você deseja para a página.
+//Crie um arquivo css para estilização. Você pode utilizar da estilização oferecida e fazer apenas os ajustes necessários, mas todo CSS da página deve ser estar nesse arquivo separado.
+//Adicione imagens para os diferentes planos.
+
+
+//Requisito 5: Página de Formulário
+
+// Implemente as funções do formulário de forma que os dados colocados nos campos capturados e apareçam no console (geralmente não se usa o console, aqui é apenas para fingirmos estarmos enviando os dados do formulário) quando o botão for clicado
+
+//Implemente verificações para que campos se houverem campos vazios no formulário este não seja "enviado", apenas o campo "Telefone" deverá ser opcional.
+
+//Implemente a estilização da página em um novo arquivo css próprio para a página do formulário mantendo a coerência com a página base que você criou.
