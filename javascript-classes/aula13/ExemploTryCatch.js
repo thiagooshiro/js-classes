f8aebb4a70c3c7c0fa0a8ccd87402698a7a788f2
thiagooshiro <oshirothiago@gmail.com>
2024-08-17 10:29:54 -0300

    aula13

--- javascript-classes/aula13/ExemploTryCatch.js
+++ javascript-classes/aula13/ExemploTryCatch.js
@@ -2,24 +2,27 @@
 
 try {
     // Tentando converter uma string em número
-    let result = parseInt("abc"); // Isso não é um número válido
-    console.log(result); // Não será executado se ocorrer um erro
+    let result = { nome:  'Mariana'}; // Isso não é um número válido
+    console.log(result.toUpperCase()); // Não será executado se ocorrer um erro
 } catch (error) {
     // Este bloco será executado se ocorrer um erro
-    console.error("Erro ao converter a string em número:", error.message);
+    // console.error(`Erro ao converter: ${error.message}`);
     // O `error.message` fornece uma mensagem sobre o erro ocorrido
 }
+
+// console.log('Bom dia Turma!')
 
 //Ex02 Quando o "try" funciona
 
 try {
-    // Tentando converter uma string em número
-    let result = parseInt("123"); // String válida para conversão
-    console.log("Resultado da conversão:", result);
-    // A conversão é bem-sucedida, então o código continua normalmente
+    //Tentando converter uma string em número
+   let result = parseInt("123"); // String válida para conversão
+   console.log("Resultado da conversão:", result);
+    //A conversão é bem-sucedida, então o código continua normalmente
 } catch (error) {
-    // Este bloco não será executado, pois não ocorreu erro
-    console.error("Erro inesperado:", error.message);
+    //Este bloco não será executado, pois não ocorreu erro
+   console.error("Erro inesperado:", error.message);
+   console.log('Olá turma 707')
 }
 
 
@@ -27,17 +30,17 @@ try {
 
 
 try {
-    // Tentando acessar um índice fora dos limites do array
-    let numbers = [1, 2, 3];
+    //Tentando acessar um índice fora dos limites do array
+   let numbers = [1, 2, 3];
     let number = numbers[5]; // Índice que não existe no array
     if (number === undefined) {
         throw new Error("Índice fora dos limites do array.");
     }
     console.log(number);
 } catch (error) {
-    // Este bloco será executado se ocorrer um erro
+    //Este bloco será executado se ocorrer um erro
     console.error("Erro ao acessar o índice do array:", error.message);
 } finally {
-    // Este bloco será sempre executado, independentemente de ter ocorrido um erro ou não
+    //Este bloco será sempre executado, independentemente de ter ocorrido um erro ou não
     console.log("Bloco finally executado.");
 }
---
+++ javascript-classes/aula13/teste.js
@@ -0,0 +1,10 @@
+
+// try {
+    let result = 1
+    console.log(result.toUpperCase())
+// } catch (error) {
+//     console.error('Não é possível conveter para caixa alta')
+// }
+
+console.log('Bom dia!')
+
