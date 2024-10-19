0b2ce4904cfe8ae5c85a8b7ddcc65d7c08ee11a7
thiagooshiro <oshirothiago@gmail.com>
2024-10-19 13:45:25 -0300

    atualizado sem resolução dos exercícios aula2

--- javascript-classes/aula1/aula1.js
+++ javascript-classes/aula1/aula1.js
@@ -4,45 +4,87 @@ let altura = 1.69;
 const idade = 21;
 const brasileiro = true;
 
-// Escrever um console.log com cada das operações matemáticas básicas:
-// Adição, subtração, multiplicação, divisão e resto.
 
-// Soma
-// console.log(42 + 33) // Quando somamos valores números, acontecem como uma soma regular.
+//1  - Crie um algoritmo que exiba na tela (terminal) a mensagem "Olá Mundo".
+console.log('Olá Mundo!')
 
-// console.log(nome + sobrenome)
+//2  - Crie um algoritmo que exiba na tela (terminal) seu nome. Por exemplo: "Olá, João Paulo."
 
-// Subtração
 
-// console.log(idade - 15)
+//3  - Crie um algoritmo que exiba na tela (terminal) sua idade. Por exemplo: 17.
 
-// Multiplicação:
 
-// console.log(altura * 2)
+//4  - Crie um algoritmo que exiba na tela (terminal) (em linhas separadas) seu nome e sua idade.
 
-// Divisão
 
-// console.log(idade/2)
+//5  - Crie um algoritmo que exiba na tela (terminal) (na mesma linha) seu nome e sua idade.
 
-// Resto 
 
-// console.log(idade % 3)
+//6  - Crie um algoritmo que exiba na tela (terminal) a soma de dois números inteiros.
+
+
+//7  - Crie um algoritmo que exiba na tela (terminal) a subtração de dois números inteiros.
+
+
+//8  - Crie um algoritmo que exiba na tela (terminal) a multiplicação de dois números inteiros.
+
+
+//9  - Crie um algoritmo que exiba na tela (terminal) a divisão de dois números inteiros.
+
+
+//10 - Crie um algoritmo que exiba na tela (terminal) o resto da divisão de dois números inteiros.
+
+
+
+//12 - Crie um algoritmo que exiba na tela (terminal) o quadrado de um número inteiro.
+
+
+//14 - Crie um algoritmo que exiba na tela (terminal) o tipo de uma variável cujo valor atribuído é "jpaulo@infinityschoool.com.br".
+
+
+// Crie um algoritmo que recebe as seguintes informações:
+//  o nome, idade, a fruta favorita e se o usuário é ou não aluno da Infinity School.
+//  Imprima os valores no seguindo o exemplo:
+//  Se as informações forem: "João", 34, "banana", as informações devem aparecer dessa forma no terminal:
+//  Nome: João
+//  Idade: 34
+//  Fruta favorita: banana
+//  Aluno Infinty: Sim  
 
 
 // Operações de comparação com operadores lógicos:
 let numero1 = 5
 let numero2 = '5'
 
-console.log(numero1 == numero2)
-console.log(numero1 === numero2)
+let verd = 5
+let verd2 = '5'
+
+console.log(verd == verd2)  //true
+console.log(numero1 === numero2) //false
+
 
 // Operador "E" ou "AND"
-
-console.log(true && false)
+console.log(false && false) // false
+console.log(false && true) // false
+console.log(true && false) // false
+console.log(true && true) // true
 
 // Operador "Ou" ou "OR"
 
-console.log(true || false)
+console.log(false || false) // false
+console.log(true || false) // true
+console.log(false || true) // true
+console.log(true || true) // true
 
 
-console.log(`O ${nome} ${sobrenome} é professor na Infinity School`)
+//Operadores de comparação
+console.log(5 > 2) //true
+console.log(10 >= 22) //false
+
+console.log(4 < 5) // true
+console.log(7 <= 7) // true
+
+
+
+
+// console.log(`O ${nome} ${sobrenome} é professor na Infinity School`)
--- javascript-classes/aula1/teste.js
+++ javascript-classes/aula1/teste.js
@@ -1,3 +1,26 @@
-let msg =  prompt('Digite algo')
+let nome = 'Gabriel'
+let altura = 1.72
+let idade = 23
+let brasileiro = true
 
-console.log(msg)
+const UM_SEGUNDO = 1000
+
+var bola = 'redonda'
+
+let numero = 2
+console.log(numero)  //2
+
+numero **= 2
+// numero = numero * 2
+console.log(numero) //2 * 2 = 4
+numero **= 2
+console.log(numero)// 
+
+numero **= 2
+console.log(numero)
+
+numero **= 2
+console.log(numero)
+
+numero **= 2
+console.log(numero)
